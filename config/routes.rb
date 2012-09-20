1cefa97c9f1b24b6b9b034dd7130f46a36d78cef
Ed Scott <eblakescott@gmail.com>
2012-09-20 14:27:31 -0500

    edited no_waiting/index.html.erb to list locations in alpha order on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ WaitTime::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'no_waiting#index', as: 'No Waiting!'
+   root :to => 'no_waiting#index', as: 'no_waiting'
 
   # See how all your routes lay out with "rake routes"
 
