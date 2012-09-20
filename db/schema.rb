8e3f8f27b20565cd6ae274765a53f318d1ff772d
Ed Scott <eblakescott@gmail.com>
2012-09-20 14:02:43 -0500

    changed column in Locations table from Type, which is reserved, to Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920181159) do
+ActiveRecord::Schema.define(:version => 20120920185045) do
 
   create_table "locations", :force => true do |t|
     t.string   "title"
-    t.string   "type"
+    t.string   "category"
     t.text     "description"
     t.string   "image_url"
     t.datetime "created_at",  :null => false
