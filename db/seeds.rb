c26b4b28234799f8a63d38b3d0a735b43cead344
Ed Scott <eblakescott@gmail.com>
2012-09-22 12:06:32 -0500

    added seed data for Museums, ran rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,3 +52,27 @@ Location.create(title: 'Chicago Restaurant',
       </p>},
   image_url: 'chicago_restaurant.jpg')
 
+#Seed data for Museums
+Museum.delete_all
+Museum.create(image_url: 'field_museum_2.jpg',
+  name: 'Field Museum',  
+description: 
+    %{<p>
+        The Field Museum of Natural History (abbreviated FMNH) is located in Chicago, Illinois, USA. It sits on Lake Shore Drive next to Lake Michigan, part of a scenic complex known as the Museum Campus Chicago. The museum collections contain over 21 million specimens, of which only a small portion are ever on display. The president of the museum is Richard W. Lariviere.
+      </p>}
+ )
+# . . .
+Museum.create(image_url: 'shedd_aquarium.jpg',
+  name: 'Shedd Aquarium',  
+description:
+    %{<p>
+        The Shedd Aquarium (formally the John G. Shedd Aquarium) is an indoor public aquarium in Chicago, Illinois in the United States that opened on May 30, 1930. The aquarium contains over 25,000 fish, and was for some time the largest indoor aquarium in the world with 5,000,000 US gallons (19,000,000 l; 4,200,000 imp gal) of water. The Shedd Aquarium was the first inland aquarium with a permanent saltwater fish collection. It is surrounded by Museum Campus Chicago, which it shares with the Adler Planetarium and the Field Museum of Natural History.
+      </p>})
+# . . .
+
+Museum.create(image_url: 'planetarium.jpg',
+  name: 'Planetarium',
+  description: 
+    %{<p>
+        The Adler Planetarium & Astronomy Museum in Chicago, Illinois was the first planetarium built in the Western Hemisphere and is the oldest in existence today. Adler was founded and built in 1930 by the philanthropist Max Adler, with the assistance of the first director of the planetarium, Philip Fox. The Adler Planetarium opened to the public on May 12, 1930. 
+      </p>})
