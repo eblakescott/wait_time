836539d3e04f64bb9dfd30b6080387bdcf6e139c
Ed Scott <eblakescott@gmail.com>
2012-09-25 07:47:59 -0500

    pushing changes that created errors in lines show view and locations show view

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,4 @@
 class Line < ActiveRecord::Base
   belongs_to :location
-  attr_accessible :location_id
+  attr_accessible :location_id, :current_wait
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   has_many :lines, dependent: :destroy
+  attr_accessible :title, :category, :image_url, :description
 end
